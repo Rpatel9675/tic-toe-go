1e6e8ad50fe97aa74b46b73221188becb02d94e1
Rpatel9675 <pprl735757@gmail.com>
2024-01-22 11:23:40 +0530

    Final stage

--- app.js
+++ app.js
@@ -1,79 +1,58 @@
-console.log("working");
-let button= document.getElementById("button");
-let input= document.querySelector("input");
-let x=1;
-let move= document.querySelector("move");
-let arr=[];
-let person1=[];
-let person2=[];
+let currentPlayer = 'X';
+let moves = 0;
 
-function checkForWin() {
-    let cells = Array.from(document.getElementsByTagName('td'));
-    let winning = [
-        [1, 2, 3], 
-        [4, 5, 6], 
-        [7, 8, 9], 
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9], 
-        [1, 5, 9], 
-        [3, 5, 7] 
-    ];
-    
+let cells = document.querySelectorAll("td");
 
-    return false;
-}
-function resetGame() {
-    Array.from(document.getElementsByTagName('td')).forEach(b => {
-        b.innerText = '';
+for (let cell of cells) {
+    cell.addEventListener("click", function () {
+        makeMove(cell.id.slice(-1));
     });
-    x = 0;
-    arr=[];
 }
-button.addEventListener('click', function(){   
-    let pos="row";
-    let newPos= pos+ input.value;
-    let b= document.getElementById(newPos);
-    if(input.value>9 || input.value<=0){
-        alert("Please, Enter a valid position: ");
-        input.value="";
-    }
-    else if(arr.includes(input.value)){
-        alert("Please, Enter a valid position: ");
-        input.value="";
-    }
-    else{
-        if (x >= 5) {
+
+function makeMove(cellNumber) {
+    let cell = document.getElementById('row' + cellNumber);
+
+    if (!cell.innerText && moves < 9) {
+        cell.innerText = currentPlayer;
+        moves++;
+
+        if (moves >= 5) {
             if (checkForWin()) {
-                alert('Player ' + x%2 + ' wins!');
+                alert('Player ' + currentPlayer + ' wins!');
                 resetGame();
                 return;
             }
         }
-
-        // Check for a tie
-        if (x === 9 && !checkForWin()) {
+        if (moves === 9 && !checkForWin()) {
             alert('It\'s a tie!');
             resetGame();
             return;
         }
-        if(x%2==0){
-            b.innerText="X";
-            x++;
-            arr.push(input.value);
-            person2.push(input.value);
-            
-            input.value="";
-            
-        }
-        else{
-            b.innerText="O";
-            x++;
-            person1.push(input.value);
-            arr.push(input.value);
-            input.value="";
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+}
+
+function checkForWin() {
+    let winningCombinations = [
+        [1, 2, 3], [4, 5, 6], [7, 8, 9],
+        [1, 4, 7], [2, 5, 8], [3, 6, 9],
+        [1, 5, 9], [3, 5, 7]
+    ];
+
+    for (let combination of winningCombinations) {
+        let [a, b, c] = combination;
+        if (cells[a - 1].innerText && cells[a - 1].innerText === cells[b - 1].innerText && cells[b - 1].innerText === cells[c - 1].innerText) {
+            return true;
         }
     }
-    
-   
-});
+
+    return false;
+}
+
+function resetGame() {
+    Array.from(cells).forEach(cell => {
+        cell.innerText = '';
+    });
+    currentPlayer = 'X';
+    moves = 0;
+}
